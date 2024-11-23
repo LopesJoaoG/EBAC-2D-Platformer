72f7d6923eba7e220877a18e52097348ad98adad
João Lopes <lopesjoaogui@gmail.com>
2024-11-23 20:25:18 -0300

    Added Jump (looks shitty)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(-.1f, 0);
 
     public float speed;
+    public float forceJump;
+
     // Update is called once per frame
     void Update()
+    {
+        Jump();
+        SideMovement();
+        
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
+        }
+    }
+
+    private void SideMovement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -20,6 +37,15 @@ public class PlayerController : MonoBehaviour
         {
             // myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.position.y);
+        }
+
+        if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+        else if (myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
         }
     }
 }
